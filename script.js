e1e4700540db8208157477bf193b29946ac4906c
kyoung3212 <kevin.young3212@gmail.com>
2018-07-17 15:25:10 -0700

    0.5 completed with functional delete button

--- script.js
+++ script.js
@@ -1,105 +1,98 @@
-/* information about jsdocs: 
-* param: http://usejsdoc.org/tags-param.html#examples
-* returns: http://usejsdoc.org/tags-returns.html
-* 
-/**
- * Listen for the document to load and initialize the application
- */
-$(document).ready();
+$(document).ready(initializeApp);
 
-/**
- * Define all global variables here.  
- */
-/***********************
- * student_array - global array to hold student objects
- * @type {Array}
- * example of student_array after input: 
- * student_array = [
- *  { name: 'Jake', course: 'Math', grade: 85 },
- *  { name: 'Jill', course: 'Comp Sci', grade: 85 }
- * ];
- */
+var studentArray = [];
 
-/***************************************************************************************************
-* initializeApp 
-* @params {undefined} none
-* @returns: {undefined} none
-* initializes the application, including adding click handlers and pulling in any data from the server, in later versions
-*/
 function initializeApp(){
+    addClickHandlersToElements();
 }
 
-/***************************************************************************************************
-* addClickHandlerstoElements
-* @params {undefined} 
-* @returns  {undefined}
-*     
-*/
 function addClickHandlersToElements(){
+    $('.add').on('click', handleAddClicked)
+    $('.cancel').on('click', handleCancelClick)
+    console.log('Add Click Handlers');
+    
+    
 }
 
-/***************************************************************************************************
- * handleAddClicked - Event Handler when user clicks the add button
- * @param {object} event  The event object from the click
- * @return: 
-       none
- */
 function handleAddClicked(){
+    console.log('handleAddClick: success')
+    if($('#studentName').val() === '' && $('#course').val() === '' && $('#studentGrade').val() === ''){
+        return;
+    }
+    addStudent();
+
+
+    
+    
 }
-/***************************************************************************************************
- * handleCancelClicked - Event Handler when user clicks the cancel button, should clear out student form
- * @param: {undefined} none
- * @returns: {undefined} none
- * @calls: clearAddStudentFormInputs
- */
+
 function handleCancelClick(){
+    clearAddStudentFormInputs();
+    console.log('handleCancelClicked: success')
 }
-/***************************************************************************************************
- * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
- * @param {undefined} none
- * @return undefined
- * @calls clearAddStudentFormInputs, updateStudentList
- */
+
+
 function addStudent(){
+    var studentName = $('#studentName').val();
+    var course = $('#course').val();
+    var grade = $('#studentGrade').val();
+    var studentObj= {
+        name: studentName,
+        course: course,
+        grade: grade
+    };
+
+    studentArray.push(studentObj);
+    console.log(studentArray);  
+    clearAddStudentFormInputs();
+
+    updateStudentList();
 }
-/***************************************************************************************************
- * clearAddStudentForm - clears out the form values based on inputIds variable
- */
+
+
 function clearAddStudentFormInputs(){
-}
-/***************************************************************************************************
- * renderStudentOnDom - take in a student object, create html elements from the values and then append the elements
- * into the .student_list tbody
- * @param {object} studentObj a single student object with course, name, and grade inside
- */
-function renderStudentOnDom(){
-}
+    console.log('Clear add student from Inputs');
+    $('input:text').val('')
 
-/***************************************************************************************************
- * updateStudentList - centralized function to update the average and call student list update
- * @param students {array} the array of student objects
- * @returns {undefined} none
- * @calls renderStudentOnDom, calculateGradeAverage, renderGradeAverage
- */
+}
 function updateStudentList(){
-  
+    renderStudentOnDom();
+    var avgGrade = calculateGradeAverage();
+    $('.avgGrade').text(avgGrade);    
 }
-/***************************************************************************************************
- * calculateGradeAverage - loop through the global student array and calculate average grade and return that value
- * @param: {array} students  the array of student objects
- * @returns {number}
- */
+
+function renderStudentOnDom(){
+    for (var i=0; i< studentArray.length; i++){
+    var tableRow = $('<tr>')
+    var studentName = $('<td>').text(studentArray[i].name);
+    var studentCourse = $('<td>').text(studentArray[i].course);
+    var studentGrade = $('<td>').text(studentArray[i].grade);
+    var deleteButton = $('<button>', {
+        class : 'btn btn-danger',
+        text: 'Delete',
+        id: `delete-${i}`,
+    });
+    deleteButton.on('click', handleDeleteButton)
+    tableRow.append(studentName, studentCourse, studentGrade, deleteButton);
+    }
+    $('tbody').append(tableRow)
+}
+
+
+
 function calculateGradeAverage(){
+    var totalGrades = 0;
+    var totalStudents = studentArray.length;
+    for (var i = 0; i < totalStudents; i++){
+        totalGrades += parseInt(studentArray[i].grade)
+    }
+    var average = totalGrades / totalStudents;
+    return average;
 }
-/***************************************************************************************************
- * renderGradeAverage - updates the on-page grade average
- * @param: {number} average    the grade average
- * @returns {undefined} none
- */
-function renderGradeAverage(){
+
+function handleDeleteButton (){
+    console.log('delete button clicked');
+    this.closest('tr').remove();
+    var removeStudent = $(this).attr('id');
+    studentArray.splice(removeStudent, 1);
 }
-
-
-
-
-
