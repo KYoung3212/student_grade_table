b8dbf0166c1e3210d74b9c54e3eb06487fa2795e
kyoung3212 <kevin.young3212@gmail.com>
2018-07-17 19:40:25 -0700

    completed v1.0 with database on click

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ var studentArray = [];
 
 function initializeApp(){
     addClickHandlersToElements();
+    getDataFromServer();    
 }
 
 function addClickHandlersToElements(){
-    $('.add').on('click', handleAddClicked)
-    $('.cancel').on('click', handleCancelClick)
+    $('.add').on('click', handleAddClicked);
+    $('.cancel').on('click', handleCancelClick);
+    $('.serverData').on('click', getDataFromServer);   
     console.log('Add Click Handlers');
     
     
@@ -20,10 +22,6 @@ function handleAddClicked(){
         return;
     }
     addStudent();
-
-
-    
-    
 }
 
 function handleCancelClick(){
@@ -45,8 +43,8 @@ function addStudent(){
     studentArray.push(studentObj);
     console.log(studentArray);  
     clearAddStudentFormInputs();
-
     updateStudentList();
+
 }
 
 
@@ -56,43 +54,80 @@ function clearAddStudentFormInputs(){
 
 }
 function updateStudentList(){
-    renderStudentOnDom();
-    var avgGrade = calculateGradeAverage();
-    $('.avgGrade').text(avgGrade);    
+    renderStudentOnDom(studentArray[studentArray.length-1]);
+    var avgGrade = calculateGradeAverage(studentArray);
+    $('.avgGrade').text(avgGrade + "%");    
 }
 
-function renderStudentOnDom(){
-    for (var i=0; i< studentArray.length; i++){
+function renderStudentOnDom(studentObj){
     var tableRow = $('<tr>')
-    var studentName = $('<td>').text(studentArray[i].name);
-    var studentCourse = $('<td>').text(studentArray[i].course);
-    var studentGrade = $('<td>').text(studentArray[i].grade);
+    var studentName = $('<td>').text(studentObj.name);
+    var studentCourse = $('<td>').text(studentObj.course);
+    var studentGrade = $('<td>').text(studentObj.grade);
     var deleteButton = $('<button>', {
         class : 'btn btn-danger',
         text: 'Delete',
-        id: `delete-${i}`,
+        id: studentObj.name
     });
-    deleteButton.on('click', handleDeleteButton)
+    deleteButton.on('click', function(){
+        this.closest('tr').remove();
+        handleDeleteButton()
+    })
     tableRow.append(studentName, studentCourse, studentGrade, deleteButton);
-    }
     $('tbody').append(tableRow)
-}
-
-
-
-function calculateGradeAverage(){
-    var totalGrades = 0;
-    var totalStudents = studentArray.length;
-    for (var i = 0; i < totalStudents; i++){
-        totalGrades += parseInt(studentArray[i].grade)
     }
-    var average = totalGrades / totalStudents;
-    return average;
+
+
+
+
+function calculateGradeAverage(array){
+    var totalGrades = 0;
+    var totalStudents = array.length;
+    for (var i = 0; i < totalStudents; i++){
+        totalGrades += parseInt(array[i].grade)
+    }
+    var averageGrade = parseInt(totalGrades / totalStudents);
+    if(isNaN(averageGrade)){
+        averageGrade = 0;
+    }    
+    return averageGrade;
 }
 
-function handleDeleteButton (){
+function handleDeleteButton (studentObj){
     console.log('delete button clicked');
-    this.closest('tr').remove();
-    var removeStudent = $(this).attr('id');
-    studentArray.splice(removeStudent, 1);
+    // this.closest('tr').remove();
+    // var removeStudent = $(this).attr('id');
+    studentArray.splice(studentObj, 1);
+    var avgGrade = calculateGradeAverage(studentArray);
+    $('.avgGrade').text(avgGrade + "%")
+
+}
+
+function getDataFromServer(){
+    var the_data = {
+        api_key: "OH4GI7VfKh"
+    }
+    var ajaxOptions = {
+        dataType: "json", 
+        data: the_data,
+        method: "POST",
+        url: "http://s-apis.learningfuze.com/sgt/get",
+
+        success: function(response){
+            var responseArray = response.data;
+            console.log(studentArray);
+            for(var i = 0; i < responseArray.length; i++){
+            studentArray = responseArray;
+            renderStudentOnDom(responseArray[i]);
+            var avgGrade = calculateGradeAverage(responseArray);
+            $(".avgGrade").text(avgGrade + "%")
+            }
+        },
+        error: failedToRetrieve        
+    }
+    $.ajax(ajaxOptions)
+}
+
+function failedToRetrieve(){
+    $('.errorModal').modal('show');
 }
