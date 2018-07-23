35d0cdcebd3b354232f615ac43e1a1c37415aae7
kyoung3212 <kevin.young3212@gmail.com>
2018-07-23 16:31:46 -0700

    cleaned up code/final

--- script.js
+++ script.js
@@ -87,140 +87,140 @@ function renderStudentOnDom(studentObj) {
         id: studentObj.id
     });
     deleteButton.on('click', function () {
-                $('#deleteConfirm').modal({
-                    show: true
-                });
+        $('#deleteConfirm').modal({
+            show: true
+        });
 
-                var closestRow = this.closest('tr');
-                console.log(closestRow)
-                $('.deleteText').text(`Are you sure you want to delete ${studentObj.name}?`)
+        var closestRow = this.closest('tr');
+        console.log(closestRow)
+        $('.deleteText').text(`Are you sure you want to delete ${studentObj.name}?`)
 
-                $('.confirmDeleteButton').on('click', function () {
-                    $(closestRow).remove();
-                    handleDeleteButton(studentObj);
-                    deleteStudent(studentObj);
-                    $('#deleteConfirm').modal('hide')
-                })
-            });
+        $('.confirmDeleteButton').on('click', function () {
+            $(closestRow).remove();
+            handleDeleteButton(studentObj);
+            deleteStudent(studentObj);
+            $('#deleteConfirm').modal('hide')
+        })
+    });
 
-                updateButton.on('click', function () {
-                    $('#updateEntry').modal({
-                        show: true
-                    });
+    updateButton.on('click', function () {
+        $('#updateEntry').modal({
+            show: true
+        });
 
-                })
-                updateButtonTd.append(updateButton);
-                deleteButtonTd.append(deleteButton);
-                tableRow.append(studentName, studentCourse, studentGrade, updateButtonTd, deleteButtonTd);
-                $('tbody').append(tableRow);
-            };
-
-
+    })
+    updateButtonTd.append(updateButton);
+    deleteButtonTd.append(deleteButton);
+    tableRow.append(studentName, studentCourse, studentGrade, updateButtonTd, deleteButtonTd);
+    $('tbody').append(tableRow);
+};
 
 
-            function calculateGradeAverage(array) {
-                var totalGrades = 0;
-                var totalStudents = array.length;
-                for (var i = 0; i < totalStudents; i++) {
-                    totalGrades += parseInt(array[i].grade)
-                }
-                var averageGrade = parseInt(totalGrades / totalStudents);
-                if (isNaN(averageGrade)) {
-                    averageGrade = 0;
-                }
-                return averageGrade;
+
+
+function calculateGradeAverage(array) {
+    var totalGrades = 0;
+    var totalStudents = array.length;
+    for (var i = 0; i < totalStudents; i++) {
+        totalGrades += parseInt(array[i].grade)
+    }
+    var averageGrade = parseInt(totalGrades / totalStudents);
+    if (isNaN(averageGrade)) {
+        averageGrade = 0;
+    }
+    return averageGrade;
+}
+
+function handleDeleteButton(studentObj) {
+    console.log('delete button clicked');
+    // this.closest('tr').remove();
+    // var removeStudent = $(this).attr('id');
+    studentArray.splice(studentObj, 1);
+    var avgGrade = calculateGradeAverage(studentArray);
+    $('.avgGrade').text(avgGrade + "%")
+    // deleteStudent(studentObj)
+
+}
+
+function getDataFromServer() {
+    var the_data = {
+        api_key: "OH4GI7VfKh"
+    }
+    var ajaxOptions = {
+        dataType: "json",
+        data: the_data,
+        method: "POST",
+        url: "https://s-apis.learningfuze.com/sgt/get",
+
+        success: function (response) {
+            var responseArray = response.data;
+            console.log(studentArray);
+            for (var i = 0; i < responseArray.length; i++) {
+                studentArray = responseArray;
+                renderStudentOnDom(responseArray[i]);
+                var avgGrade = calculateGradeAverage(responseArray);
+                $(".avgGrade").text(avgGrade + "%")
             }
+        },
+        error: failedToRetrieve
+    }
+    $.ajax(ajaxOptions)
+}
 
-            function handleDeleteButton(studentObj) {
-                console.log('delete button clicked');
-                // this.closest('tr').remove();
-                // var removeStudent = $(this).attr('id');
-                studentArray.splice(studentObj, 1);
-                var avgGrade = calculateGradeAverage(studentArray);
-                $('.avgGrade').text(avgGrade + "%")
-                // deleteStudent(studentObj)
+function sendToServer(studentObj) {
+    var the_data = {
+        api_key: "OH4GI7VfKh",
+        name: studentObj.name,
+        course: studentObj.course,
+        grade: studentObj.grade,
+        student_id: parseInt(studentObj.id)
+    }
+    var ajaxOptions = {
+        dataType: "json",
+        data: the_data,
+        method: "POST",
+        url: "https://s-apis.learningfuze.com/sgt/create",
 
-            }
+        success: function () {
+            console.log("Success: Student Add to Serve")
+        },
+        error: failedToRetrieve
 
-            function getDataFromServer() {
-                var the_data = {
-                    api_key: "OH4GI7VfKh"
-                }
-                var ajaxOptions = {
-                    dataType: "json",
-                    data: the_data,
-                    method: "POST",
-                    url: "https://s-apis.learningfuze.com/sgt/get",
+    }
+    $.ajax(ajaxOptions);
+}
 
-                    success: function (response) {
-                        var responseArray = response.data;
-                        console.log(studentArray);
-                        for (var i = 0; i < responseArray.length; i++) {
-                            studentArray = responseArray;
-                            renderStudentOnDom(responseArray[i]);
-                            var avgGrade = calculateGradeAverage(responseArray);
-                            $(".avgGrade").text(avgGrade + "%")
-                        }
-                    },
-                    error: failedToRetrieve
-                }
-                $.ajax(ajaxOptions)
-            }
+function deleteStudent(studentObj) {
+    var the_data = {
+        api_key: "OH4GI7VfKh",
+        student_id: parseInt(studentObj.id)
 
-            function sendToServer(studentObj) {
-                var the_data = {
-                    api_key: "OH4GI7VfKh",
-                    name: studentObj.name,
-                    course: studentObj.course,
-                    grade: studentObj.grade,
-                    student_id: parseInt(studentObj.id)
-                }
-                var ajaxOptions = {
-                    dataType: "json",
-                    data: the_data,
-                    method: "POST",
-                    url: "https://s-apis.learningfuze.com/sgt/create",
+    }
+    var ajaxOptions = {
+        dataType: "json",
+        data: the_data,
+        method: "POST",
+        url: "https://s-apis.learningfuze.com/sgt/delete",
 
-                    success: function () {
-                        console.log("Success: Student Add to Serve")
-                    },
-                    error: failedToRetrieve
+        success: function () {
+            console.log("Success: Student Add to Serve")
+        },
+        error: failedToRetrieve
 
-                }
-                $.ajax(ajaxOptions);
-            }
-
-            function deleteStudent(studentObj) {
-                var the_data = {
-                    api_key: "OH4GI7VfKh",
-                    student_id: parseInt(studentObj.id)
-
-                }
-                var ajaxOptions = {
-                    dataType: "json",
-                    data: the_data,
-                    method: "POST",
-                    url: "https://s-apis.learningfuze.com/sgt/delete",
-
-                    success: function () {
-                        console.log("Success: Student Add to Serve")
-                    },
-                    error: failedToRetrieve
-
-                }
-                $.ajax(ajaxOptions);
-            }
+    }
+    $.ajax(ajaxOptions);
+}
 
 
-            function failedToRetrieve() {
-                $('.errorModal').modal('show');
-            }
+function failedToRetrieve() {
+    $('.errorModal').modal('show');
+}
 
-            function loadSpinner() {
-                $(document).ajaxStart(function () {
-                    $('.fa-spin').show();
-                })
-                $(document).ajaxComplete(function () {
-                    $('.fa-spin').hide();
-                })
-            }
+function loadSpinner() {
+    $(document).ajaxStart(function () {
+        $('.fa-spin').show();
+    })
+    $(document).ajaxComplete(function () {
+        $('.fa-spin').hide();
+    })
+}
